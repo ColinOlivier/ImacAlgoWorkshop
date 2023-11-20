730fd4226f9c0e0a31bb2369ff613f9a9d58931e
Jules Fouchy <jules.fouchy@ntymail.com>
2023-11-20 21:25:16 +0100

    Add effect that removes red

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 int main()
 {
     sil::Image image{"images/logo.png"};
-    // TODO: modifier l'image
+    for(glm::vec3& color : image.pixels())
+        color.r = 0.f;
     image.save("output/pouet.png");
 }
