79bd66dbaa34a20b0cfeabfb15365d3bc75f4fba
ColinOlivier <55018721+ColinOlivier@users.noreply.github.com>
2023-11-22 10:07:49 +0100

    add convolution solution

--- src/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <sil/sil.hpp>
-
-int main()
-{
-    sil::Image image{"images/logo.png"};
-    for(glm::vec3& color : image.pixels())
-        color.r = 0.f;
-    image.save("output/pouet.png");
-}
